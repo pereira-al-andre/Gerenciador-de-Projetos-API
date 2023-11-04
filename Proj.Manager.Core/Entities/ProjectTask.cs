62898c06851b959013efeaced914e845d777bf08
Andre <pereira.al.andre@gmail.com>
2023-11-04 13:44:17 -0300

    Minor changes

---
+++ Proj.Manager.Core/Entities/ProjectTask.cs
@@ -0,0 +1,99 @@
+﻿using Proj.Manager.Core.Enums;
+using Proj.Manager.Core.Exceptions.Common;
+using Proj.Manager.Core.Primitives;
+using Proj.Manager.Core.ValueObjects;
+
+namespace Proj.Manager.Core.Entities
+{
+    public sealed class ProjectTask : Entity
+    {
+        internal ProjectTask()
+        {
+            
+        }
+
+        internal ProjectTask(
+            Project project,
+            Name name, 
+            Description description)
+        {
+            this.ProjectId = project.Id;
+            this.Name = name;
+            this.Description = description;
+
+        }
+
+        public Guid ProjectId { get; private set; }
+        public Name Name { get; private set; } = null!;
+        public Description Description { get; private set; } = null!;
+        public DateTime? StartDate { get; private set; }
+        public DateTime? FinishDate { get; private set; }
+        public Enums.TaskStatus Status { get; private set; } = Enums.TaskStatus.ToDo;
+        public List<Member> Members { get; private set; } = new();
+        public Project Project { get; private set; } = null!;
+
+        public bool IsToDo => Status == Enums.TaskStatus.ToDo;
+        public bool IsOnGoing => Status == Enums.TaskStatus.OnGoing;
+        public bool IsCompleted => Status == Enums.TaskStatus.Completed;
+        public bool IsDeleted => Status == Enums.TaskStatus.Deleted;
+
+        public void Update(
+           Name? name = null,
+           Description? description = null)
+        {
+
+            if (IsCompleted)
+                throw new DomainLayerException(DomainExceptionType.InvalidTaskStatus, "You can not update a completed task.");
+
+            if (IsDeleted)
+                throw new DomainLayerException(DomainExceptionType.InvalidTaskStatus, "You can not update a deleted task.");
+
+            if (name != null) this.Name = name;
+            if (description != null) this.Description = description;
+        }
+        public void AddMember(Member member)
+        {
+            if (IsCompleted)
+                throw new DomainLayerException(DomainExceptionType.InvalidTaskStatus, "You can not add members to a completed task.");
+
+            if (IsDeleted)
+                throw new DomainLayerException(DomainExceptionType.InvalidTaskStatus, "You can not add members to a deleted task.");
+
+            this.Members.Add(member);
+        }
+        public void RemoveMember(Member member)
+        {
+            if (IsCompleted)
+                throw new DomainLayerException(DomainExceptionType.InvalidTaskStatus, "You can not remove members from a completed task.");
+
+            if (IsDeleted)
+                throw new DomainLayerException(DomainExceptionType.InvalidTaskStatus, "You can not remove members from a deleted task.");
+
+            this.Members.Remove(member);
+
+            if (this.Members.Count == 0) this.Status = Enums.TaskStatus.ToDo;
+        }
+        public void Delete()
+        {
+            if (IsDeleted || IsCompleted) return;
+
+            this.Status = Enums.TaskStatus.Deleted;
+            this.FinishDate = DateTime.Now;
+        }
+        public void Complete()
+        {
+            if (!IsOnGoing) return;
+
+            this.Status = Enums.TaskStatus.Completed;
+            this.FinishDate = DateTime.Now;
+        }
+        public void Start()
+        {
+            if (!IsToDo)
+                throw new DomainLayerException(DomainExceptionType.InvalidTaskStatus, "You can not start tasks that are not in 'todo' status.");
+
+            this.Status = Enums.TaskStatus.OnGoing;
+            this.StartDate = DateTime.Now;
+        }
+    }
+}
---
+++ Proj.Manager.Core/Enums/DomainExceptionType.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace Proj.Manager.Core.Enums
+{
+    public enum DomainExceptionType
+    {
+        [Description("Invalid e-mail value passed")]
+        InvalidEmail = 1,
+
+        [Description("Invalid name value passed")]
+        InvalidName = 2,
+
+        [Description("Invalid description value passed")]
+        InvalidDescription = 3,
+
+        [Description("Invalid password value passed")]
+        InvalidPassword = 4,
+
+        [Description("Invalid project status")]
+        InvalidProjectStatus = 5,
+
+        [Description("Invalid task status")]
+        InvalidTaskStatus = 6,
+
+        [Description("ProjectTask not found in the project")]
+        TaskNotFoundInTheProject = 7,
+    }
+}
